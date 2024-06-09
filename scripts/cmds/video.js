01ea8c5db099eb63eed74edee6f34daa1684daea
abhishekdahal09 <abishekdon932@gmail.com>
2024-06-09 23:15:43 +0545

    Update video.js

--- scripts/cmds/video.js
+++ scripts/cmds/video.js
@@ -1,129 +1,84 @@
 const axios = require("axios");
-const fs = require('fs-extra');
-const path = require('path');
-const ytdl = require("ytdl-core");
-const yts = require("yt-search");
-
-async function checkAuthor(authorName) {
-  try {
-    const response = await axios.get('https://author-check.vercel.app/name');
-    const apiAuthor = response.data.name;
-    return apiAuthor === authorName;
-  } catch (error) {
-    console.error("Error checking author:", error);
-    return false;
-  }
-}
-
-async function sing(api, event, args, message) {
-  api.setMessageReaction("🕢", event.messageID, (err) => {}, true);
-  try {
-    let title = '';
-
-    const extractShortUrl = async () => {
-      const attachment = event.messageReply.attachments[0];
-      if (attachment.type === "video" || attachment.type === "audio") {
-        return attachment.url;
-      } else {
-        throw new Error("Invalid attachment type.");
-      }
-    };
-
-    if (event.messageReply && event.messageReply.attachments && event.messageReply.attachments.length > 0 && args.length === 0) {
-      const shortUrl = await extractShortUrl();
-      const musicRecognitionResponse = await axios.get(`https://audio-recom.onrender.com/kshitiz?url=${encodeURIComponent(shortUrl)}`);
-      title = musicRecognitionResponse.data.title;
-    } else if (args.length > 0 && args[0] !== 'video') {
-      title = args.join(" ");
-    } else {
-      message.reply("");
-      return;
-    }
-
-    const searchResults = await yts(title);
-    if (!searchResults.videos.length) {
-      message.reply("No song found for the given query.");
-      return;
-    }
-
-    const videoUrl = searchResults.videos[0].url;
-    const stream = await ytdl(videoUrl, { filter: "audioandvideo" });
-
-    const fileName = `video_${Date.now()}.mp4`;
-    const filePath = path.join(__dirname, "cache", fileName);
-    const writer = fs.createWriteStream(filePath);
-
-    stream.pipe(writer);
-
-    writer.on('finish', async () => {
-      try {
-        const audioStream = fs.createReadStream(filePath);
-        const sentMessage = await message.reply({ body: `🎧 Playing: ${title}`, attachment: audioStream });
-        api.setMessageReaction("✅", event.messageID, () => {}, true);
-
-        global.GoatBot.onReply.set(sentMessage.messageID, {
-          commandName: singCommand.name,
-          uid: event.senderID
-        });
-      } catch (error) {
-        console.error('Error sending message:', error.message);
-        message.reply("An error occurred while sending the audio file.");
-      } finally {
-        await fs.unlink(filePath);
-      }
-    });
-
-    writer.on('error', (error) => {
-      console.error("Error:", error);
-      message.reply("An error occurred while processing the audio file.");
-    });
-  } catch (error) {
-    console.error("Error:", error);
-    message.reply("An error occurred while processing the request.");
-  }
-}
-
-function handleReply(api, event, args, message) {
-  const replyData = global.GoatBot.onReply.get(event.messageReply.messageID);
-
-  if (replyData && replyData.uid === event.senderID) {
-    global.GoatBot.onReply.delete(event.messageReply.messageID);
-    const newArgs = event.body.split(" ");
-    return sing(api, event, newArgs, message);
-  }
-}
-
-const singCommand = {
-  name: "video",
-  version: "2.0",
-  author: "Vex_Kshitiz",
-  countDown: 10,
-  role: 0,
-  shortDescription: "play video from yt",
-  longDescription: "play video from yt support audio recognition.",
-  category: "video",
-  guide: "{p}video {musicName} or reply to audio or video by {p}video"
-};
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
-  config: singCommand,
-  handleCommand: sing,
-  onStart: async function ({ api, event, message, args }) {
-    const isAuthorValid = await checkAuthor(module.exports.config.author);
-    if (!isAuthorValid) {
-      await message.reply("Author changer alert! This command belongs to Vex_Kshitiz.");
-      return;
+  config: {
+    name: "video", 
+    aliases: ["vd"], 
+
+    
+    author: "Vex_Kshitiz",
+
+    
+    version: "1.0",
+    cooldowns: 10,
+    role: 0,
+    shortDescription: "",
+    longDescription: "Get tiktok videos.",
+    category: "fun",
+    guide: "{p}tuktuk",
+  },
+
+  onStart: async function ({ api, event, message }) {
+    function getRandomUsername() {
+
+      const usernames = ['san_dip011','sagareditz19','imntmax', ',mahadev.1690o','luci_editz7'];
+      
+      // you can add multiple usernames too if you want to get random videos from random users.
+
+      const randomIndex = Math.floor(Math.random() * usernames.length);
+      return usernames[randomIndex];
     }
 
-    return sing(api, event, args, message);
-  },
-  onReply: function ({ api, message, event, args }) {
-    if (event.type === 'message_reply') {
-      if (event.messageReply.attachments && event.messageReply.attachments.length > 0 && event.body.trim() === 'video') {
-        return sing(api, event, [], message);
-      } else if (event.messageReply.body && event.messageReply.body.trim() !== '') {
-        return handleReply(api, event, args, message);
-      }
+    api.setMessageReaction("😻", event.messageID, (err) => {}, true);
+
+    try {
+      const username = getRandomUsername();
+      const response = await axios.get(`https://tuktuk-scrap.onrender.com/kshitiz?username=${username}`);
+      const user = response.data.user || "@user_unknown";
+      const postData = response.data.posts;
+      const selectedUrl = getRandomUrl(postData);
+
+      const videoResponse = await axios.get(selectedUrl, { responseType: "stream" });
+
+      const tempVideoPath = path.join(__dirname, "cache", `tuktuk.mp4`);
+      const writer = fs.createWriteStream(tempVideoPath);
+      videoResponse.data.pipe(writer);
+
+      writer.on("finish", async () => {
+        const stream = fs.createReadStream(tempVideoPath);
+        await message.reply({
+          body: ``,
+          attachment: stream,
+        });
+        api.setMessageReaction("🤍", event.messageID, (err) => {}, true);
+        fs.unlink(tempVideoPath, (err) => {
+          if (err) console.error(err);
+          console.log(`Deleted`);
+        });
+      });
+    } catch (error) {
+      console.error(error);
+      message.reply("Sorry, an error occurred.");
     }
   }
 };
+
+let usedUrls = [];
+
+function getRandomUrl(postData) {
+  if (usedUrls.length === postData.length) {
+    usedUrls = [];
+  }
+
+  let randomIndex;
+  let selectedPost;
+  do {
+    randomIndex = Math.floor(Math.random() * postData.length);
+    selectedPost = postData[randomIndex].replace(/\\/g, "/");
+  } while (usedUrls.includes(selectedPost));
+
+  usedUrls.push(selectedPost);
+  return selectedPost;
+        }
