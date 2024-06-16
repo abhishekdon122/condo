db1d38859d33909f096817791a36be10f27bab22
abhishekdahal09 <abishekdon932@gmail.com>
2024-06-16 15:23:03 +0545

    Update anu.js

--- scripts/cmds/anu.js
+++ scripts/cmds/anu.js
@@ -17,8 +17,8 @@ onChat: async function({
 }) {
 	if (event.body && event.body.toLowerCase() == "bot") return message.reply("yes.. 🥳❤️‍🩹");
 	if (event.body && event.body.toLowerCase() == "good morning") return message.reply("good morning..🌄❤️‍🩹");
-	if (event.body && event.body.toLowerCase() == "good evening") return message.reply("good evening🌃❤️‍🩹");
-	if (event.body && event.body.toLowerCase() == "good afternoon") return message.reply("good afternoon ❤️‍🩹");
+	if (event.body && event.body.toLowerCase() == "abhishek") return message.reply("Girlfriend Sanga Bg Hunuxa");
+	if (event.body && event.body.toLowerCase() == "lado") return message.reply("tei ley tero aama lai garyera ta yetro vako hos😘😈");
 	if (event.body && event.body.toLowerCase() == "good night") return message.reply("good night 💤🌙");
 	if (event.body && event.body.toLowerCase() == "hi") return message.reply("yes...🥳");
 	if (event.body && event.body.toLowerCase() == "hello") return message.reply("◡̈⋆ʜᴇʟʟᴏ(●’◡’●)ﾉ");
