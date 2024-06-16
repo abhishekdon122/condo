d4ca578750c3f4c3ed57d78924c4f276b23ad1f1
abhishekdahal09 <abishekdon932@gmail.com>
2024-06-16 09:16:09 +0545

    Update autolink.js

--- scripts/cmds/autolink.js
+++ scripts/cmds/autolink.js
@@ -2,7 +2,7 @@ const fs = require("fs-extra");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const qs = require("qs");
-const { getStreamFromURL, shortenURL, randomString } = global.utils;
+const { getStreamFromURL, randomString } = global.utils;
 
 function loadAutoLinkStates() {
   try {
@@ -13,21 +13,34 @@ function loadAutoLinkStates() {
   }
 }
 
+
 function saveAutoLinkStates(states) {
   fs.writeFileSync("autolink.json", JSON.stringify(states, null, 2));
 }
 
+
 let autoLinkStates = loadAutoLinkStates();
+
+
+async function shortenURL(url) {
+  try {
+    const response = await axios.get(`https://shortner-sepia.vercel.app/kshitiz?url=${encodeURIComponent(url)}`);
+    return response.data.shortened;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to shorten URL");
+  }
+}
 
 module.exports = {
   threadStates: {},
   config: {
     name: 'autolink',
-    version: '4.0',
+    version: '5.0',
     author: 'Vex_Kshitiz',
     countDown: 5,
     role: 0,
-    shortDescription: 'Auto video downloader for Instagram, Facebook, TikTok, Twitter, pinterest and youtube',
+    shortDescription: 'Auto video downloader for Instagram, Facebook, TikTok, Twitter, Pinterest, and YouTube',
     longDescription: '',
     category: 'media',
     guide: {
@@ -67,7 +80,7 @@ module.exports = {
       } else {
         api.sendMessage("", event.threadID, event.messageID);
       }
-      api.setMessageReaction("🕐", event.messageID, (err) => {}, true);
+      api.setMessageReaction("✨", event.messageID, (err) => {}, true);
     }
   },
   downLoad: function (url, api, event) {
@@ -103,7 +116,6 @@ module.exports = {
 
       const shortUrl = await shortenURL(res);
       const messageBody = `✅ 🔗 Download Url: ${shortUrl}`;
-
       api.sendMessage({
         body: messageBody,
         attachment: fs.createReadStream(path)
@@ -436,4 +448,4 @@ async function fbDownloader(url) {
       success: false
     };
   }
-}
+    }
