0e23dde167b994e62e4544f7ab6fa4307d81838d
abhishekdonnna <terobau00999@gmail.com>
2024-07-01 14:51:48 +0545

    Update sikshya.js

--- scripts/cmds/sikshya.js
+++ scripts/cmds/sikshya.js
@@ -23,7 +23,7 @@ if (event.body && event.body.toLowerCase() == "bubu give") return message.reply(
 if (event.body && event.body.toLowerCase() == "muji") return message.reply("hajur muji🤣🤣 ");
 if (event.body && event.body.toLowerCase() == "k gardai xau") return message.reply("Hagdai xu 😓");
 	if (event.body && event.body.toLowerCase() == "good morning") return message.reply("good morning..🌄❤️‍🩹");
-	if (event.body && event.body.toLowerCase() == "abhishek") return message.reply("girlfriend sanga dating gaxa 😈");
+	if (event.body && event.body.toLowerCase() == "abhishek") return message.reply("hagdai xa 😝😝");
 if (event.body && event.body.toLowerCase() == "lado") return message.reply("chus dim 💋😋");
 if (event.body && event.body.toLowerCase() == "💋") return message.reply("sasss ganayo brush garyeera aaije😈");
 if (event.body && event.body.toLowerCase() == "k xa") return message.reply("gf ra Paisa bahek sab xa 😭");
@@ -32,7 +32,7 @@ if (event.body && event.body.toLowerCase() == "🙄") return message.reply("yeta
 if (event.body && event.body.toLowerCase() == "bubu") return message.reply("aaije mero Khana 😥");
 	if (event.body && event.body.toLowerCase() == "good night") return message.reply("good night 💤🌙");
 if (event.body && event.body.toLowerCase() == "sut") return message.reply("tero budi patha sutdinxu😈🤍");
-if (event.body && event.body.toLowerCase() == "bot") return message.reply("hjr vannus .help to see my cmds🤍");
+if (event.body && event.body.toLowerCase() == "bot") return message.reply("hjr babe 🤍");
 	if (event.body && event.body.toLowerCase() == "hi") return message.reply("yes...🥳");
 	if (event.body && event.body.toLowerCase() == "hello") return message.reply("◡̈⋆ʜᴇʟʟᴏ(●’◡’●)ﾉ");
 	if (event.body && event.body.toLowerCase() == "how are you") return message.reply("fine and you..?");
