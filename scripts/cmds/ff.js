79a56cb56c02dc022e171326bb6d5df41a8eda29
Rinadon009 <rinakarki0098@gmail.com>
2024-07-04 14:33:53 +0545

    Update ff.js

--- scripts/cmds/ff.js
+++ scripts/cmds/ff.js
@@ -1,32 +1,36 @@
-const axios = require('axios');
+.cmd install ff.js const axios = require('axios');
 
 module.exports = {
   config: {
     name: "ff",
+    aliases: ["freefire","ffvideo"],
     version: "1.0",
-    author: "Vex_Kshitiz",
+    author: "Vex_Kshitiz | Fahim_Noob", 
     countDown: 5,
     role: 0,
-    longDescription: "lauda reels.",
-    category: "FF Video",
-    guide: "{p}ffreels",
+    longDescription: "freefire video.",
+    category: "video",
+    guide: "{p}freefire",
   },
 
   onStart: async function ({ api, event, message }) {
-    api.setMessageReaction("✨", event.messageID, () => {}, true);
-
     try {
+      api.setMessageReaction("✨", event.messageID, () => {}, true);
+
       const { data } = await axios.get("https://smfahim.onrender.com/ff/apikey=puti");
-      if (data.code !== 200) throw new Error("api issue");
+      if (data.code !== 200) throw new Error("API issue");
 
       const fileId = data.url.match(/\/d\/(.+?)\//)[1];
       const directUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
+      
       const { data: videoStream } = await axios.get(directUrl, { responseType: 'stream' });
 
-      message.reply({ body: ``, attachment: videoStream });
+      await message.reply({ body: ``, attachment: videoStream });
+
+      api.setMessageReaction("✅", event.messageID, () => {}, true);
     } catch (error) {
       console.error('Error', error);
-      message.reply({ body: "error" });
+      message.reply({ body: "Error occurred while processing your request" });
     }
   },
 };
