c493bdc1c0325ee86b87f49b72ff72f986c73162
Rinadon009 <rinakarki0098@gmail.com>
2024-07-04 14:31:48 +0545

    Update ffreels.js

--- scripts/cmds/ffreels.js
+++ scripts/cmds/ffreels.js
@@ -1,50 +1,36 @@
 const axios = require('axios');
-const fs = require('fs');
- 
+
 module.exports = {
   config: {
     name: "ffreels",
-    aliases: ["ffreel"],
+    aliases: ["fffreel","ffreelsvideo"],
     version: "1.0",
-    author: "Fahim the Noob",
+    author: "Vex_Kshitiz | Fahim_Noob", 
+    countDown: 5,
     role: 0,
-    shortDescription: {
-      en: "Send a anime reels video"
-    },
-    longDescription: {
-      en: "Fetches an ff reels video from the provided API, downloads it, and sends it to the chat"
-    },
+    longDescription: "freefire reels video.",
     category: "video",
-    guide: {
-      en: "Type 'ffreels' or 'ffreel' to get an anime video."
+    guide: "{p}ffreels",
+  },
+
+  onStart: async function ({ api, event, message }) {
+    try {
+      api.setMessageReaction("✨", event.messageID, () => {}, true);
+
+      const { data } = await axios.get("https://smfahim.onrender.com/ffreels/apikey=puti");
+      if (data.code !== 200) throw new Error("API issue");
+
+      const fileId = data.url.match(/\/d\/(.+?)\//)[1];
+      const directUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
+      
+      const { data: videoStream } = await axios.get(directUrl, { responseType: 'stream' });
+
+      await message.reply({ body: ``, attachment: videoStream });
+
+      api.setMessageReaction("✅", event.messageID, () => {}, true);
+    } catch (error) {
+      console.error('Error', error);
+      message.reply({ body: "Error occurred while processing your request" });
     }
   },
-  onStart: async function ({ api, message, event }) {
-    try {
-      const response = await axios.get('https://ff-reels.onrender.com/video/apikey=smkhang');
-      const videoUrl = response.data.url;
-      message.reaction("🕑", event.messageID); 
-      const videoResponse = await axios.get(videoUrl, { responseType: 'stream' });
-      const videoStream = fs.createWriteStream('video.mp4');
-      videoResponse.data.pipe(videoStream);
-      
-      await new Promise((resolve, reject) => {
-        videoStream.on('finish', resolve);
-        videoStream.on('error', reject);
-      });
-        message.reaction("✅", event.messageID);
-      await message.reply({
-        body: 'Here is your ff reels video:',
-        attachment: fs.createReadStream('video.mp4')
-      }, event.threadID);
-    
-      fs.unlinkSync('video.mp4');
-      
-      
-    } catch (error) {
-      console.error(error);
-      await api.sendMessage(`Sorry, there was an error fetching the video.`, event.threadID);
-      await message.reaction("❌", event.messageID);
-    }
-  }
 };
