4f31ad5120e934caf6a89d085216529e2cc3b3ef
mujii-hub <ramkrishnashiwakoti09@gmail.com>
2024-07-09 09:19:07 +0545

    Update ping.js

--- scripts/cmds/ping.js
+++ scripts/cmds/ping.js
@@ -1,71 +1,25 @@
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
 module.exports = {
   config: {
     name: "ping",
-    aliases: [],
+    aliases: ["ms"],
     version: "1.0",
-    author: "EDINST",
-    countDown: 10,
+    author: "Sandu",
     role: 0,
     shortDescription: {
-      en: ""
+      en: "Displays the current ping of the bot's system."
     },
     longDescription: {
-      en: ""
+      en: "Displays the current ping of the bot's system."
     },
-    category: "tools",
+    category: "System",
     guide: {
-      en: ""
+      en: "Use {p}ping to check the current ping of the bot's system."
     }
   },
-
-  langs: {
-    en: {
-      gg: ""
-    }
-  },
-
-  onStart: async function ({ api, event, args, message }) {
-    try {
-      const sendPingCheckMessage = async () => {
-        const startTime = Date.now();
-        await delay(500);
-        const botLatency = Date.now() - startTime;
-        const apiLatency = message.timestamp - startTime;
-
-        let pingStatus = "";
-
-        if (botLatency >= 500) {
-          pingStatus = "slow";
-        } else if (botLatency >= 300 && botLatency < 500) {
-          pingStatus = "normal";
-        } else if (botLatency < 300) {
-          pingStatus = "fast";
-        }
-
-        api.sendMessage({
-          body: `Ping status: ${pingStatus} \nPing: ${botLatency}ms`,
-          attachment: null,
-          mentions: [{
-            tag: event.senderID,
-            id: event.senderID,
-            fromIndex: 6,
-          }]
-        }, event.threadID, (error, messageInfo) => {
-          setTimeout(() => {
-            api.deleteMessage(messageInfo.messageID);
-          }, 500);
-        });
-      };
-
-      message.reply("checking ping...", event.threadID, async (error, messageInfo) => {
-        await delay(500);
-        await sendPingCheckMessage();
-        api.deleteMessage(messageInfo.messageID);
-      });
-    } catch (error) {
-      console.error(error);
-    }
+  onStart: async function ({ api, event, args }) {
+    const timeStart = Date.now();
+    await api.sendMessage("Checking Bot's ping", event.threadID);
+    const ping = Date.now() - timeStart;
+    api.sendMessage(`The current ping is ${ping}ms.`, event.threadID);
   }
 };
