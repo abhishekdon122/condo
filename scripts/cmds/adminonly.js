e2c3fb717dd70942a2e9558909ee4f7b41d6816a
abhishekdon122 <niteshdon145@gmail.com>
2024-07-10 20:47:26 +0545

    Update adminonly.js

--- scripts/cmds/adminonly.js
+++ scripts/cmds/adminonly.js
@@ -5,7 +5,7 @@ const { client } = global;
 module.exports = {
 	config: {
 		name: "adminonly",
-		aliases: ["adonly", "onlyad", "onlyadmin"],
+		aliases: ["adonly", "adm", "onlyadmin"],
 		version: "1.5",
 		author: "NTKhang",
 		countDown: 5,
