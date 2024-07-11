0dcab2f463b2c94461560c16b87cbbd10b5a8c65
abhishekdon122 <niteshdon145@gmail.com>
2024-07-11 20:08:16 +0545

    Update puri.js

--- scripts/cmds/puri.js
+++ scripts/cmds/puri.js
@@ -1,10 +1,10 @@
-const axios = require ("axios");
-const fs = require ("fs-extra");
+const axios = require("axios");
+const fs = require("fs-extra");
 
 module.exports = {
   config: {
     name: "puri",
-    aliases: [puri2],
+    aliases: [""], // Corrected alias syntax
     version: "1.0",
     author: "OTINXSANDIP",
     countDown: 5,
@@ -16,40 +16,52 @@ module.exports = {
   },
 
   onStart: async function({ api, event, threadsData, usersData }) {
-
-    const { threadID, messageID, senderID } = event;
+    const { threadID, senderID } = event;
     const { participantIDs } = await api.getThreadInfo(threadID);
+    
+    // Generate a random percentage for demonstration
     var tle = Math.floor(Math.random() * 101);
-    var namee = (await usersData.get(senderID)).name
+    
+    // Get sender's and a random participant's names
+    var namee = (await usersData.get(senderID)).name;
     const botID = api.getCurrentUserID();
     const listUserID = participantIDs.filter(ID => ID != botID && ID != senderID);
     var id = listUserID[Math.floor(Math.random() * listUserID.length)];
-    var name = (await usersData.get(id)).name
-    var arraytag = [];
-    arraytag.push({ id: senderID, tag: namee });
-    arraytag.push({ id: id, tag: name });
+    var name = (await usersData.get(id)).name;
+    
+    // Array to store tags
+    var arraytag = [
+      { id: senderID, tag: namee },
+      { id: id, tag: name }
+    ];
 
-    let Avatar = (await axios.get(`https://graph.facebook.com/${senderID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(__dirname + "/cache/avt.png", Buffer.from(Avatar, "utf-8"));
+    // Fetch sender's and participant's avatars and save them locally
+    await Promise.all([
+      axios.get(`https://graph.facebook.com/${senderID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" })
+        .then(response => fs.writeFileSync(__dirname + "/cache/avt.png", Buffer.from(response.data, "binary"))),
+      axios.get(`https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" })
+        .then(response => fs.writeFileSync(__dirname + "/cache/avt2.png", Buffer.from(response.data, "binary")))
+    ]);
 
-    let gifLove = (await axios.get(`https://i.ibb.co/y4dWfQq/image.gif`, { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(__dirname + "/cache/giflove.png", Buffer.from(gifLove, "utf-8"));
+    // Fetch and save the love gif locally
+    let gifLove = await axios.get(`https://i.ibb.co/y4dWfQq/image.gif`, { responseType: "arraybuffer" });
+    fs.writeFileSync(__dirname + "/cache/giflove.gif", Buffer.from(gifLove.data, "binary"));
 
-    let Avatar2 = (await axios.get(`https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(__dirname + "/cache/avt2.png", Buffer.from(Avatar2, "utf-8"));
+    // Prepare array of read streams for attachments
+    var imglove = [
+      fs.createReadStream(__dirname + "/cache/avt.png"),
+      fs.createReadStream(__dirname + "/cache/giflove.gif"),
+      fs.createReadStream(__dirname + "/cache/avt2.png")
+    ];
 
-    var imglove = [];
-
-    imglove.push(fs.createReadStream(__dirname + "/cache/avt.png"));
-    imglove.push(fs.createReadStream(__dirname + "/cache/giflove.png"));
-    imglove.push(fs.createReadStream(__dirname + "/cache/avt2.png"));
-
+    // Prepare the message object to send
     var msg = {
-      body: `Successful pairing!\n😻Wish you two hundred years of happiness\n💋Double ratio: ${tle}%\n${namee} 🤍 ${name}`,
+      body: `Successful pairing!\n😻 Wish you two hundred years of happiness\n💋 Double ratio: ${tle}%\n${namee} 🤍 ${name}`,
       mentions: arraytag,
       attachment: imglove
     };
 
+    // Send the message with attachments
     return api.sendMessage(msg, event.threadID, event.messageID);
   }
 };
