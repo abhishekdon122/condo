9b921388157341f064876b5611cfb3998c4d1d61
siksheyy <abhishekdon009988@gmail.com>
2024-07-15 06:15:49 +0545

    Update video.js

--- scripts/cmds/video.js
+++ scripts/cmds/video.js
@@ -3,6 +3,18 @@ const fs = require('fs-extra');
 const path = require('path');
 const { getStreamFromURL, shortenURL, randomString } = global.utils;
 
+const API_KEYS = [
+    'b38444b5b7mshc6ce6bcd5c9e446p154fa1jsn7bbcfb025b3b',
+    '719775e815msh65471c929a0203bp10fe44jsndcb70c04bc42',
+    
+    'a2743acb5amsh6ac9c5c61aada87p156ebcjsnd25f1ef87037',
+    '8e938a48bdmshcf5ccdacbd62b60p1bffa7jsn23b2515c852d',
+    'f9649271b8mshae610e65f24780cp1fff43jsn808620779631',
+    '8e906ff706msh33ffb3d489a561ap108b70jsne55d8d497698',
+
+    '4bd76967f9msh2ba46c8cf871b4ep1eab38jsn19c9067a90bb',
+];
+
 async function video(api, event, args, message) {
     api.setMessageReaction("✨", event.messageID, (err) => {}, true);
     try {
@@ -19,11 +31,16 @@ async function video(api, event, args, message) {
             }
         };
 
+        const getRandomApiKey = () => {
+            const randomIndex = Math.floor(Math.random() * API_KEYS.length);
+            return API_KEYS[randomIndex];
+        };
+
         if (event.messageReply && event.messageReply.attachments && event.messageReply.attachments.length > 0) {
             shortUrl = await extractShortUrl();
-            const musicRecognitionResponse = await axios.get(`https://audio-recom.onrender.com/kshitiz?url=${encodeURIComponent(shortUrl)}`);
+            const musicRecognitionResponse = await axios.get(`https://audio-recon-ahcw.onrender.com/kshitiz?url=${encodeURIComponent(shortUrl)}`);
             title = musicRecognitionResponse.data.title;
-            const searchResponse = await axios.get(`https://youtube-kshitiz.vercel.app/youtube?search=${encodeURIComponent(title)}`);
+            const searchResponse = await axios.get(`https://youtube-kshitiz-gamma.vercel.app/yt?search=${encodeURIComponent(title)}`);
             if (searchResponse.data.length > 0) {
                 videoId = searchResponse.data[0].videoId;
             }
@@ -34,12 +51,12 @@ async function video(api, event, args, message) {
             return;
         } else {
             title = args.join(" ");
-            const searchResponse = await axios.get(`https://youtube-kshitiz.vercel.app/youtube?search=${encodeURIComponent(title)}`);
+            const searchResponse = await axios.get(`https://youtube-kshitiz-gamma.vercel.app/yt?search=${encodeURIComponent(title)}`);
             if (searchResponse.data.length > 0) {
                 videoId = searchResponse.data[0].videoId;
             }
 
-            const videoUrlResponse = await axios.get(`https://youtube-kshitiz.vercel.app/download?id=${encodeURIComponent(videoId)}`);
+            const videoUrlResponse = await axios.get(`https://yt-kshitiz.vercel.app/download?id=${encodeURIComponent(videoId)}&apikey=${getRandomApiKey()}`);
             if (videoUrlResponse.data.length > 0) {
                 shortUrl = await shortenURL(videoUrlResponse.data[0]);
             }
@@ -50,8 +67,8 @@ async function video(api, event, args, message) {
             return;
         }
 
-        const downloadResponse = await axios.get(`https://youtube-kshitiz.vercel.app/download?id=${encodeURIComponent(videoId)}`);
-        const videoUrl = downloadResponse.data[0]; 
+        const downloadResponse = await axios.get(`https://yt-kshitiz.vercel.app/download?id=${encodeURIComponent(videoId)}&apikey=${getRandomApiKey()}`);
+        const videoUrl = downloadResponse.data[0];
 
         if (!videoUrl) {
             message.reply("Failed to retrieve download link for the video.");
@@ -68,7 +85,7 @@ async function video(api, event, args, message) {
         response.data.pipe(writer);
 
         writer.on('finish', () => {
-            const videoStream = fs.createReadStream(path.join(__dirname, "cache", `${videoId}.mp4`)); 
+            const videoStream = fs.createReadStream(path.join(__dirname, "cache", `${videoId}.mp4`));
             message.reply({ body: ``, attachment: videoStream });
             api.setMessageReaction("✅", event.messageID, () => {}, true);
         });
@@ -87,7 +104,7 @@ module.exports = {
     config: {
         name: "video", 
         version: "1.0",
-        author: "Kshitiz",
+        author: "Vex_kshitiz",
         countDown: 10,
         role: 0,
         shortDescription: "play video from youtube",
