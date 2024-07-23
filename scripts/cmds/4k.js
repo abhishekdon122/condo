86a542fd39bb38efa49e4836d5adb5a499d7e4ed
Hakerhoni <dahalsunita306@gmail.com>
2024-07-23 14:44:26 +0545

    Update 4k.js

--- scripts/cmds/4k.js
+++ scripts/cmds/4k.js
@@ -8,7 +8,7 @@ module.exports = {
     version: "1.0",
     author: "JARiF",
     countDown: 15,
-    role: 0,
+    role: 2,
     longDescription: "Upscale your image.",
     category: "image",
     guide: {
